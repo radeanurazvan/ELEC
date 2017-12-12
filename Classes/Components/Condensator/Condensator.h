941c4f4bbc4263147420acb9ac1dac195563cb71
Cucu-nElec <marian_vladut52@yahoo.com>
2017-12-12 15:31:48 +0200

    Created Condensator

---
+++ Classes/Components/Condensator/Condensator.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "d:\fac\PROJ\ELEC\Classes\Components\BaseComponent\BaseComponent.h"
+class Condensator :
+	public BaseComponent
+{
+public:
+	Condensator();
+	~Condensator();
+};
+
