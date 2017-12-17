d0b2db290b71989b8dc8743cd641eb8433bd2383
Cucu-nElec <marian_vladut52@yahoo.com>
2017-12-17 23:49:11 +0200

    Created Battery Draw

--- Classes/Components/Battery/Battery.cpp
+++ Classes/Components/Battery/Battery.cpp
@@ -1,5 +1,6 @@
 #include "Battery.h"
-
+#include "../../Helpers/GraphicsHelper/GraphicsHelper.h"
+#include "Resources/BatteryResources.h"
 
 
 Battery::Battery()
@@ -8,4 +9,14 @@ Battery::Battery()
 
 void Battery::Draw()
 {
+	const auto leftConductorPoint = GetReferencePoint();
+	auto rightConductorPoint = GetReferencePoint();
+	rightConductorPoint.MoveToRight(BatteryResources::spaceBetweenConductors);
+	rightConductorPoint.MoveUpwards(BatteryResources::conductorRightHeight);
+
+	GraphicsHelper::DrawSegmnentOfHeight(leftConductorPoint, BatteryResources::conductorLeftHeight);
+	GraphicsHelper::DrawSegmnentOfHeight(rightConductorPoint, -BatteryResources::conductorRightHeight);
+
+	DrawMiddleConnectors(leftConductorPoint, rightConductorPoint);
+
 }
---
+++ Classes/Components/Battery/Resources/BatteryResources.h
@@ -0,0 +1,12 @@
+#pragma once
+class BatteryResources
+{
+private:
+	BatteryResources();
+public:
+	static const int spaceBetweenConductors = 10;
+	static const int conductorRightHeight = 15;
+	static const int conductorLeftHeight = 25;
+	
+
+};
