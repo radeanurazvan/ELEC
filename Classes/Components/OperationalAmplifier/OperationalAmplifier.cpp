46040e52ada853c3a66fdec0f5ae49d4b83a369b
Cucu-nElec <marian_vladut52@yahoo.com>
2017-12-24 16:21:19 +0200

    Created OperationalAmplifier Draw

--- Classes/Components/OperationalAmplifier/OperationalAmplifier.cpp
+++ Classes/Components/OperationalAmplifier/OperationalAmplifier.cpp
@@ -1,6 +1,6 @@
 #include "OperationalAmplifier.h"
-
-
+#include "Resources//OprerationalAmplifierResources.h"
+#include "../../Helpers/GraphicsHelper/GraphicsHelper.h"
 
 OperationalAmplifier::OperationalAmplifier()
 {
@@ -9,4 +9,31 @@ OperationalAmplifier::OperationalAmplifier()
 
 void OperationalAmplifier::Draw()
 {
+	const auto StartPoint = GetReferencePoint();
+	auto EndPoint = StartPoint;
+	EndPoint.MoveToRight(OperationalAmplifierResources::TriangleWidth);
+	auto PointA = StartPoint;
+	auto PointB = EndPoint;
+	auto PointC = StartPoint;
+	PointA.MoveDownwards(OperationalAmplifierResources::TriangleHeight / 2);
+	PointC.MoveUpwards(OperationalAmplifierResources::TriangleHeight / 2);
+	GraphicsHelper::DrawTriangle(PointA, PointB, PointC);
+	auto line1 = PointA;
+	auto line2 = PointC;
+	line1.MoveUpwards(OperationalAmplifierResources::LeftConnectors);
+	line2.MoveDownwards(OperationalAmplifierResources::LeftConnectors);
+	GraphicsHelper::DrawSegmnentOfWidth(EndPoint, OperationalAmplifierResources::RightLine);
+	GraphicsHelper::DrawSegmnentOfWidth(line1, OperationalAmplifierResources::LeftLine);
+	GraphicsHelper::DrawSegmnentOfWidth(line2, OperationalAmplifierResources::LeftLine);
+	auto plus = PointA;
+	auto minus = PointC;
+	plus.MoveToRight(-OperationalAmplifierResources::LeftConnectors);
+	minus.MoveDownwards(OperationalAmplifierResources::LeftConnectors / 2);
+	minus.MoveToRight(-OperationalAmplifierResources::LeftConnectors);
+	GraphicsHelper::DrawSegmnentOfWidth(minus, -OperationalAmplifierResources::LeftConnectors);
+	GraphicsHelper::DrawSegmnentOfWidth(plus, -OperationalAmplifierResources::LeftConnectors);
+	plus.MoveToRight(-OperationalAmplifierResources::LeftConnectors / 2);
+	GraphicsHelper::DrawSegmnentOfHeight(plus, OperationalAmplifierResources::LeftConnectors / 2);
+	GraphicsHelper::DrawSegmnentOfHeight(plus, -OperationalAmplifierResources::LeftConnectors / 2);
+
 }
---
+++ Classes/Components/OperationalAmplifier/Resources/OprerationalAmplifierResources.h
@@ -0,0 +1,11 @@
+#pragma once
+class OperationalAmplifierResources{
+private:
+	OperationalAmplifierResources();
+public:
+	static const int TriangleWidth = 15;
+	static const int TriangleHeight = 25;
+	static const int LeftLine = -12;
+	static const int RightLine = 8;
+	static const int LeftConnectors = 4;
+};
