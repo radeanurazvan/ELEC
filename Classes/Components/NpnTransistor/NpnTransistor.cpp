d2af48b3598417678ada51a00ed363d98a8dd18d
Cucu-nElec <marian_vladut52@yahoo.com>
2017-12-24 21:03:03 +0200

    Created NpnTransistor

--- Classes/Components/NpnTransistor/NpnTransistor.cpp
+++ Classes/Components/NpnTransistor/NpnTransistor.cpp
@@ -1,11 +1,31 @@
 #include "NpnTransistor.h"
-
-
+#include "../../Helpers/GraphicsHelper/GraphicsHelper.h"
+#include "Resources/NpnTransistorResources.h"
 
 NpnTransistor::NpnTransistor()
 {
-}
+	
+};
+
 
 void NpnTransistor::Draw()
 {
+	const auto StartPoint = GetReferencePoint();
+	GraphicsHelper::DrawCircle(StartPoint, NpnTransistorResources::Radius);
+	auto UpperConductor = StartPoint;
+	UpperConductor.MoveDownwards(NpnTransistorResources::DistanceBetweenCondcutors);
+	UpperConductor.MoveToRight(NpnTransistorResources::DistanceFromMidPoint);
+	auto LowerConductor = StartPoint;
+	LowerConductor.MoveUpwards(NpnTransistorResources::DistanceBetweenCondcutors);
+	LowerConductor.MoveToRight(NpnTransistorResources::DistanceFromMidPoint);
+	auto MidConductor = StartPoint;
+	MidConductor.MoveToRight(-NpnTransistorResources::DistanceFromMidPoint);
+	MidConductor.MoveDownwards(NpnTransistorResources::Radius);
+	GraphicsHelper::DrawSegmnentOfHeight(MidConductor,NpnTransistorResources::Height);
+	MidConductor.MoveUpwards(NpnTransistorResources::Radius);
+	auto Line = MidConductor;
+	Line.MoveToRight(-NpnTransistorResources::Lenght);
+	GraphicsHelper::DrawLine(MidConductor, Line);
+	GraphicsHelper::DrawLine(MidConductor, UpperConductor);
+	GraphicsHelper::DrawLine(MidConductor, LowerConductor);
 }
---
+++ Classes/Components/NpnTransistor/Resources/NpnTransistorResources.h
@@ -0,0 +1,11 @@
+#pragma once
+class NpnTransistorResources{
+private:
+	NpnTransistorResources();
+public:
+	static const int Radius = 12;
+	static const int DistanceBetweenCondcutors = 15;
+	static const int DistanceFromMidPoint = 7;
+	static const int Height = 2*Radius;
+	static const int Lenght = 20;
+};
